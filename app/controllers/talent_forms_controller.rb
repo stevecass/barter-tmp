e886a256c031c8b1b96cf9b6e7977b34b0619667
Steven Cassidy <steven@mumsnet.com>
2015-04-22 00:19:28 -0400

    Update talent forms controller not to specify acl

--- app/controllers/talent_forms_controller.rb
+++ app/controllers/talent_forms_controller.rb
@@ -2,9 +2,9 @@ class TalentFormsController < ApplicationController
     skip_before_action :set_default_response_format
 
   def new
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201)
     @talent = Talent.new
-    render text: @s3_direct_post.fields.to_json
+    render text: @s3_direct_post.fields.to_json.html_safe
   end
 
 end
